c6cb55695003fb8cf7e3db19bc5389752aaea56f
Chris Tsantiris <chris.tsantiris@gmail.com>
2026-08-30 12:46:03 -0400

    Re balance difficulty levels

--- src/core/difficulty.c
+++ src/core/difficulty.c
@@ -25,6 +25,26 @@ static const DifficultyDef DIFFICULTIES[DIFF_COUNT] = {
         .id = DIFF_DONT_HURT_ME,
         .name = "DONT HURT ME.",
         .enabled = 1,
+        .enemy_health_multiplier = 0.875f,
+        .enemy_speed_multiplier = 0.90f,
+        .enemy_sight_multiplier = 0.90f,
+        .enemy_damage_multiplier = 0.80f,
+        .health_pickup_amount = 33,
+        .ammo_pickup_amount = 12,
+        .weapon_unlock_spare_magazines = 3,
+        .level_start_health_floor = 100,
+        .kill_score = 88,
+        .boss_max_health = 1400,
+        .miniboss_max_health = 831,
+        .miniboss_support_count = 2,
+        .miniboss_wave_count = 2,
+        .boss_support_count = 3,
+        .boss_wave_count = 2
+    },
+    [DIFF_BRING_EM_ON] = {
+        .id = DIFF_BRING_EM_ON,
+        .name = "BRING EM ON!",
+        .enabled = 1,
         .enemy_health_multiplier = 1.0f,
         .enemy_speed_multiplier = 1.0f,
         .enemy_sight_multiplier = 1.0f,
@@ -41,9 +61,9 @@ static const DifficultyDef DIFFICULTIES[DIFF_COUNT] = {
         .boss_support_count = 4,
         .boss_wave_count = 3
     },
-    [DIFF_BRING_EM_ON] = {
-        .id = DIFF_BRING_EM_ON,
-        .name = "BRING EM ON!",
+    [DIFF_I_AM_DEATH_INCARNATE] = {
+        .id = DIFF_I_AM_DEATH_INCARNATE,
+        .name = "I AM DEATH INCARNATE!",
         .enabled = 1,
         .enemy_health_multiplier = 1.15f,
         .enemy_speed_multiplier = 1.15f,
@@ -60,26 +80,6 @@ static const DifficultyDef DIFFICULTIES[DIFF_COUNT] = {
         .miniboss_wave_count = 4,
         .boss_support_count = 5,
         .boss_wave_count = 4
-    },
-    [DIFF_I_AM_DEATH_INCARNATE] = {
-        .id = DIFF_I_AM_DEATH_INCARNATE,
-        .name = "I AM DEATH INCARNATE!",
-        .enabled = 1,
-        .enemy_health_multiplier = 1.30f,
-        .enemy_speed_multiplier = 1.30f,
-        .enemy_sight_multiplier = 1.40f,
-        .enemy_damage_multiplier = 1.90f,
-        .health_pickup_amount = 10,
-        .ammo_pickup_amount = 12,
-        .weapon_unlock_spare_magazines = 1,
-        .level_start_health_floor = 50,
-        .kill_score = 200,
-        .boss_max_health = 2080,
-        .miniboss_max_health = 1235,
-        .miniboss_support_count = 5,
-        .miniboss_wave_count = 5,
-        .boss_support_count = 6,
-        .boss_wave_count = 5
     }
 };
 
